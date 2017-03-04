fd2a310d56e41b044465b7deb08ee902656f2524
alexanderma <infoedy@libero.it>
2017-03-04 19:46:50 +0100

    .
    *Aggiunto oggetto Quadrato
    *Agginuto ossatura Script Interazione Tuoch su lato

--- SpaceShape/Assets/Generate_Board.cs
+++ SpaceShape/Assets/Generate_Board.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 public class Generate_Board : MonoBehaviour {
 
 	public GameObject panel;
+	public GameObject Quadrato;
 	public Vector2 cellSize;
 	public int row_num;
 	public int col_num;
@@ -17,7 +18,7 @@ public class Generate_Board : MonoBehaviour {
 	{
 		CreateGrid (panel.transform.position.x, panel.transform.position.y);
 	}
-	
+
 	public void CreateGrid(float x, float y)
 	{
 		GameObject piece = (GameObject)Instantiate (panel, new Vector3 (x, y, 0f), panel.transform.rotation);
